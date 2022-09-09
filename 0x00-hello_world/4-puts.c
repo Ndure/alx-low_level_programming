c051012769b04e8e2a19fa447a564ddad4632f0a
Nicholas Ndure Maina <ndurenicholas@gmail.com>
2022-09-09 12:54:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-/**start code
-*/
 #include <stdio.h>
-/**
-* Entry- Main
-*Return 0 if successful
-*/
-int main(void)
-{
+/** 
+* main - main block 
+* Return: 0 
+*/ 
+int main(void) 
+{ 
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
+
