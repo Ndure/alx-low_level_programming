d24cf38f59d788da7ccd98691d32a53efca4c353
Nicholas Ndure Maina <ndurenicholas@gmail.com>
2022-09-14 11:51:21 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,28 @@
-#include "../holberton.h"
+/*
+ * File: 0-putchar.c
+ * Auth:Nicholas Ndure Maina
+ *
+ */
+
+#include "0-putchar.c"
 
 /**
- * main - check the code for Holberton School students.
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_alphabet();
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
